f8f289ffddeb5aee44216ff8d00a173f9a34087b
Maheswar Reddy <maheswar.dtaoofficial@gmail.com>
2025-02-19 18:16:58 +0530

    My New Website Is There !

---
+++ Mahesh/src/test/java/com/app/mahesh/MaheshApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.mahesh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaheshApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
